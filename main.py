f28c26ca4870525999fdbf8818c60d55b3d69bfd
tamali2807 <tamali2807@gmail.com>
2025-02-19 23:42:58 +0530

    Adding the filter functionality

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from create_stories import create_stories, init_db
 from trends_visualization import trends_visualization
 
 # Initialize the database
-# init_db()
+init_db()
 
 # App title
 st.title("Scrum Board App")
@@ -11,10 +11,10 @@ st.title("Scrum Board App")
 # Tab layout
 tab1, tab2 = st.tabs(["Create Stories", "Trends Visualization"])
 
-# # Tab 1: Create Stories
-# with tab1:
-#     # create_stories()
+# Tab 1: Create Stories
+with tab1:
+    create_stories()
 
-# # Tab 2: Trends Visualization
-# with tab2:
-#     trends_visualization()
+# Tab 2: Trends Visualization
+with tab2:
+    trends_visualization()
